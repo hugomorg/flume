1353112b96f8dfeb9f010c71254b687d9548bb3f
Hugo <hpjm37@gmail.com>
2021-09-27 21:42:39 +0100

    Add new/0 and run/3

--- lib/flume.ex
+++ lib/flume.ex
@@ -1,18 +1,64 @@
 defmodule Flume do
   @moduledoc """
-  Documentation for `Flume`.
+  A convenient way to handle control flow in pipelines. This makes for easier reading and composability.
   """
 
+  @type t :: %__MODULE__{}
+  @type tag :: atom()
+  @type callback_fun :: (map() -> {:ok, tag()} | {:error, atom()})
+
+  defstruct results: %{}, errors: %{}, halted: false
+
   @doc """
-  Hello world.
+  Returns empty Flume struct.
 
   ## Examples
 
-      iex> Flume.hello()
-      :world
+      iex> Flume.new()
+      %Flume{}
 
   """
-  def hello do
-    :world
+  @spec new() :: t()
+  def new do
+    %__MODULE__{}
+  end
+
+  @doc """
+  Executes passed in callback synchronously - and stores the returned result.
+
+  Callback has to be a 1-arity function, and is passed the current accumulated results.
+  It must return a `{:ok, result}` or a `{:error, reason}` tuple. In the first case,
+  the result will be added to the accumulator, and in the second case the error will be stored.
+
+  A tag annotates the operation.
+
+  ## Examples
+
+      Flume.new()
+      |> Flume.run(:a, fn _ -> {:ok, 2} end)
+      |> Flume.run(:b, fn data -> {:ok, 2 * data.a} end)
+      |> Flume.run(:this_fails, fn _ -> {:error, :for_some_reason} end)
+      |> Flume.run(:this_wont_run, fn _ -> raise "boom" end)
+
+  """
+  @spec run(t(), tag(), callback_fun) :: t()
+  def run(%Flume{halted: true} = flume, _tag, _fun), do: flume
+
+  def run(%Flume{results: results, errors: errors} = flume, tag, fun)
+      when is_atom(tag) and is_function(fun, 1) do
+    case fun.(results) do
+      {:ok, result} ->
+        results = Map.put(results, tag, result)
+        %Flume{flume | results: results}
+
+      {:error, error} ->
+        errors = Map.put(errors, tag, error)
+        %Flume{flume | errors: errors, halted: true}
+
+      bad_match ->
+        raise "Expected either an `{:ok, result}` or {:error, reason} tuple, but got #{
+                inspect(bad_match)
+              }"
+    end
   end
 end
--- test/flume_test.exs
+++ test/flume_test.exs
@@ -2,7 +2,33 @@ defmodule FlumeTest do
   use ExUnit.Case
   doctest Flume
 
-  test "greets the world" do
-    assert Flume.hello() == :world
+  test "new/0 returns empty struct" do
+    assert Flume.new() == %Flume{results: %{}, errors: %{}, halted: false}
+  end
+
+  describe "run" do
+    test "run/3 executes callback and result accumulated" do
+      flume =
+        Flume.new()
+        |> Flume.run(:a, fn _ -> {:ok, 2} end)
+        |> Flume.run(:b, fn data -> {:ok, 2 * data.a} end)
+
+      assert flume == %Flume{results: %{a: 2, b: 4}, errors: %{}, halted: false}
+    end
+
+    test "run/3 stops at first error" do
+      flume =
+        Flume.new()
+        |> Flume.run(:a, fn _ -> {:ok, 2} end)
+        |> Flume.run(:b, fn data -> {:ok, 2 * data.a} end)
+        |> Flume.run(:this_fails, fn _ -> {:error, :for_some_reason} end)
+        |> Flume.run(:this_wont_run, fn _ -> raise "boom" end)
+
+      assert flume == %Flume{
+               results: %{a: 2, b: 4},
+               errors: %{this_fails: :for_some_reason},
+               halted: true
+             }
+    end
   end
 end
