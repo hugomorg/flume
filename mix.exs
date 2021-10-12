c368686a1e519acad5befe0b05d2fe4f477bdbcb
Hugo <hpjm37@gmail.com>
2021-10-12 17:59:56 +0100

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Flume.MixProject do
   def project do
     [
       app: :flume,
-      version: "0.7.0",
+      version: "1.0.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
